f71ba5f93929163de816125fc3ffca4c126336d9
jbijlsma <jeroen_work@yahoo.com>
2023-01-17 08:37:22 +0800

    Move code to RuleSetFileReader for better reuse

--- Wavecell.RuleEngine/RuleSetFileReader.cs
+++ Wavecell.RuleEngine/RuleSetFileReader.cs
@@ -5,8 +5,10 @@ public abstract class RuleSetFileReader<TFilterValues> where TFilterValues : IFi
     private readonly IRuleSetFileLoader _loader;
     
     protected abstract string ExpectedHeader { get; }
-    protected abstract Rule<TFilterValues> CreateRule(string line);
+    protected abstract int OutputValueIndex { get; }
 
+    protected abstract TFilterValues CreateFilterValues(string[] lineParts);
+    
     protected RuleSetFileReader(IRuleSetFileLoader loader)
     {
         _loader = loader;
@@ -18,5 +20,19 @@ public abstract class RuleSetFileReader<TFilterValues> where TFilterValues : IFi
         _loader.ValidateHeader(ExpectedHeader);
 
         return _loader.Lines.Select(CreateRule);
+    }
+    
+    private Rule<TFilterValues> CreateRule(string line)
+    {
+        var parts = line.Split(',');
+        
+        var ruleId = int.Parse(parts[0]);
+        var priority = ushort.Parse(parts[1]);
+        
+        var outputValue = string.IsNullOrWhiteSpace(parts[5]) ? (int?)null : int.Parse(parts[OutputValueIndex]);
+
+        var filters = CreateFilterValues(parts);
+
+        return new Rule<TFilterValues>(ruleId, priority, outputValue, filters);
     }
 }
